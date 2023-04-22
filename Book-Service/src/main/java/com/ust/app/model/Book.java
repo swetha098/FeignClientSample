b5de26f828b40b6c395fab5d8c0bc02a6d5f96fa
Swetha S <swetha2342840@gmail.com>
2023-04-22 14:42:21 +0530

    22-4-23

---
+++ Book-Service/src/main/java/com/ust/app/model/Book.java
@@ -0,0 +1,14 @@
+package com.ust.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private Integer bookId;
+    private String bookName;
+    private Double bookCost;
+}
---
+++ ServiceRegistory/src/main/java/com/ust/micro/ServiceRegistoryApplication.java
@@ -0,0 +1,16 @@
+package com.ust.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistoryApplication.class, args);
+	}
+
+}
